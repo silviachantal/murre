e6f7d251cfd46ae7740aa908dd40debb1b7c88b7
silviachantal <80984939+silviachantal@users.noreply.github.com>
2025-01-15 22:50:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["natas", "OpenNMT-tf>=2.18.1", "OpenNMT-py==2.3.0"],
+    install_requires=["natas==1.0.1", "OpenNMT-tf>=2.18.1", "OpenNMT-py==2.3.0"],
    # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
